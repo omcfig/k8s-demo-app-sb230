bfb4f5aad1da63370a4ccdf869aa3dc66e53f280
omcfig <omcfig@gmail.com>
2020-05-22 16:11:23 -0700

    Initial commit

---
+++ src/main/java/com/example/k8sdemoappsb230/K8sDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.k8sdemoappsb230;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class K8sDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
